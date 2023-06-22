6b98e6b7297f57d2256b54c6c876b44446e086f6
Shuang Liang <129134672+Sean19899999@users.noreply.github.com>
2023-06-22 10:27:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,133 +6,126 @@ import sys
 import os
 from urllib import request
 import urllib
-# pip install pygtrans -i https://pypi.org/simple
-# ref:https://zhuanlan.zhihu.com/p/390801784
-# ref:https://beautifulsoup.readthedocs.io/zh_CN/latest/
-# ref:https://pygtrans.readthedocs.io/zh_CN/latest/langs.html
-# client = Translate()
-# text = client.translate('Google Translate')
-# print(text.translatedText)  # 谷歌翻译
 import hashlib
+import datetime
+import time
+from rfeed import *
+import feedparser
+
+# Function to get MD5 value
 def get_md5_value(src):
     _m = hashlib.md5()
     _m.update(src.encode('utf-8'))
     return _m.hexdigest()
-    
+
+# Functions to get current time and subtitle of a feed item
+def getTime(e):
+    try:
+        struct_time = e.published_parsed
+    except:
+        struct_time = time.localtime()
+    return datetime.datetime(*struct_time[:6])
+
+def getSubtitle(e):
+    try:
+        sub = e.subtitle
+    except:
+        sub = ""
+    return sub
+
+# Class to translate feed items' title and summary
+class GoogleTran:
+    def __init__(self, url, source='auto', target='zh-CN'):
+        self.url = url
+        self.source = source
+        self.target = target
+
+        self.d = feedparser.parse(self.url)
+        self.GT = Translate()
+
+    def tr(self, content):
+        tt = self.GT.translate(content, target=self.target, source=self.source)
+        try:
+            return tt.translatedText
+        except:
+            return ""
+
+    def get_newcontent(self, max=2):
+        item_list = []
+        if len(self.d.entries) < max:
+            max = len(self.d.entries)
+        for entry in self.d.entries[:max]:
+            one = Item(
+                title=self.tr(entry.title),
+                link=entry.link,
+                description=self.tr(entry.summary),
+                guid=Guid(entry.link),
+                pubDate=getTime(entry))
+            item_list.append(one)
+        feed = self.d.feed
+        newfeed = Feed(
+            title=self.tr(feed.title),
+            link=feed.link,
+            description=self.tr(getSubtitle(feed)),
+            lastBuildDate=getTime(feed),
+            items=item_list)
+        return newfeed.rss()
+
+# Reading configuration
 config = configparser.ConfigParser()
-config.read('test.ini',encoding='utf-8')
-secs=config.sections()
+config.read('test.ini', encoding='utf-8')
+secs = config.sections()
 
+# Functions to get and set config
+def get_cfg(sec, name):
+    return config.get(sec, name).strip('"')
 
+def set_cfg(sec, name, value):
+    config[sec][name] = '"%s"' % value
 
-def get_cfg(sec,name):
-    return config.get(sec,name).strip('"')
-
-def set_cfg(sec,name,value):
-    config[sec][name]='"%s"'%value
-
+# Function to get source and target languages
 def get_cfg_tra(sec):
-    cc=config.get(sec,"action").strip('"')
-    target=""
-    source=""
+    cc = config.get(sec, "action").strip('"')
     if cc == "auto":
         source  = 'auto'
         target  = 'zh-CN'
-        
-         
     else:
         source  = cc.split('->')[0]
         target  = cc.split('->')[1]
-    return source,target
+    return source, target
 
-
-# config['url']={'url':'www.baidu.com'} #类似于字典操作
- 
-# with open('example.ini','w') as configfile:
-#     config.write(configfile)
-
-BASE=get_cfg("cfg",'base')
+# Directory for output
+BASE = get_cfg("cfg", 'base')
 try:
     os.makedirs(BASE)
 except:
     pass
-links=[]
+
+links = []
 def tran(sec):
-    out_dir= BASE + get_cfg(sec,'name')
-    url=get_cfg(sec,'url')
-    max_item=int(get_cfg(sec,'max'))
-    old_md5=get_cfg(sec,'md5')
-    source,target=get_cfg_tra(sec)
+    out_dir = BASE + get_cfg(sec, 'name')
+    url = get_cfg(sec, 'url')
+    max_item = int(get_cfg(sec, 'max'))
+    old_md5 = get_cfg(sec, 'md5')
+    source, target = get_cfg_tra(sec)
     global links
 
-    links+=[" - %s [%s](%s) -> [%s](%s)\n"%(sec,url,url,get_cfg(sec,'name'),out_dir)]
+    links += [" - %s [%s](%s) -> [%s](%s)\n" % (sec, url, url, get_cfg(sec, 'name'), out_dir)]
 
-
-    GT = Translate()
-    headers={
-        'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.137 Safari/537.36 LBBROWSER'
-        }
-    req = urllib.request.Request(url, headers=headers)
-
- 
-    html_doc=request.urlopen(req).read().decode('utf8')
-    new_md5= get_md5_value(html_doc)
+    new_md5 = get_md5_value(url) # new md5 value based on url
 
     if old_md5 == new_md5:
         return
     else:
-        set_cfg(sec,'md5',new_md5)
-    # move style
-    html_doc=html_doc.replace('<?', '</s')
-    html_doc=html_doc.replace('?>', '/>')
-    
-    soup = BeautifulSoup(html_doc, 'html.parser')
-    items=soup.find_all('item')
-    for idx,e in enumerate(items):
-        if idx >max_item:
-                e.decompose()
-    
-    content= str(soup)
-    
-    content=content.replace('<title', '<stitle')
-    content=content.replace('title>', 'stitle>')
-    content=content.replace( '<pubdate>','<pubDate><span translate="no">')
-    content=content.replace( '</pubdate>','</span></pubdate>')
-    # print(content)
-    
-    
-    _text = GT.translate(content,target=target,source=source)
-    
-    
-    with open(out_dir,'w',encoding='utf-8') as f:
-        c=_text.translatedText
-        
-        c=c.replace('<stitle', '<title')
-        c=c.replace('stitle>', 'title>')
-        c=c.replace('<span translate="no">', '')
-        c=c.replace('</span></pubdate>', '</pubDate>') # 对于ttrss需要为pubDate才会识别正确
-        c=c.replace('&gt','>') # &gt 会影响识别
-        
+        set_cfg(sec, 'md5', new_md5)
+
+    c = GoogleTran(url, target=target, source=source).get_newcontent(max=max_item)
+
+    with open(out_dir, 'w', encoding='utf-8') as f:
         f.write(c)
-        #print(c)
-        #f.write(content)
-    print("GT: "+ url +" > "+ out_dir)
+        print("GT: "+ url +" > "+ out_dir)
 
-for x in secs[1:]:
-    tran(x)
-    print(config.items(x))
-
-with open('test.ini','w') as configfile:
-    config.write(configfile)
-
-
-
-YML="README.md"
-
-f = open(YML, "r+", encoding="UTF-8")
-list1 = f.readlines()           
-list1= list1[:13] + links
-
-f = open(YML, "w+", encoding="UTF-8")
-f.writelines(list1)
-f.close()
+for sec in secs:
+    if sec == "cfg":
+        continue
+    tran(sec)
