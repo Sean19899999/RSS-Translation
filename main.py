3dcb755c0180cb43d1e348b880f8ba6860a688c3
Shuang Liang <129134672+Sean19899999@users.noreply.github.com>
2023-06-22 10:34:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,126 +6,107 @@ import sys
 import os
 from urllib import request
 import urllib
+# pip install pygtrans -i https://pypi.org/simple
+# ref:https://zhuanlan.zhihu.com/p/390801784
+# ref:https://beautifulsoup.readthedocs.io/zh_CN/latest/
+# ref:https://pygtrans.readthedocs.io/zh_CN/latest/langs.html
 import hashlib
-import datetime
-import time
-from rfeed import *
-import feedparser
 
-# Function to get MD5 value
 def get_md5_value(src):
     _m = hashlib.md5()
     _m.update(src.encode('utf-8'))
     return _m.hexdigest()
-
-# Functions to get current time and subtitle of a feed item
-def getTime(e):
-    try:
-        struct_time = e.published_parsed
-    except:
-        struct_time = time.localtime()
-    return datetime.datetime(*struct_time[:6])
-
-def getSubtitle(e):
-    try:
-        sub = e.subtitle
-    except:
-        sub = ""
-    return sub
-
-# Class to translate feed items' title and summary
-class GoogleTran:
-    def __init__(self, url, source='auto', target='zh-CN'):
-        self.url = url
-        self.source = source
-        self.target = target
-
-        self.d = feedparser.parse(self.url)
-        self.GT = Translate()
-
-    def tr(self, content):
-        tt = self.GT.translate(content, target=self.target, source=self.source)
-        try:
-            return tt.translatedText
-        except:
-            return ""
-
-    def get_newcontent(self, max=2):
-        item_list = []
-        if len(self.d.entries) < max:
-            max = len(self.d.entries)
-        for entry in self.d.entries[:max]:
-            one = Item(
-                title=self.tr(entry.title),
-                link=entry.link,
-                description=self.tr(entry.summary),
-                guid=Guid(entry.link),
-                pubDate=getTime(entry))
-            item_list.append(one)
-        feed = self.d.feed
-        newfeed = Feed(
-            title=self.tr(feed.title),
-            link=feed.link,
-            description=self.tr(getSubtitle(feed)),
-            lastBuildDate=getTime(feed),
-            items=item_list)
-        return newfeed.rss()
-
-# Reading configuration
+    
 config = configparser.ConfigParser()
-config.read('test.ini', encoding='utf-8')
-secs = config.sections()
+config.read('test.ini',encoding='utf-8')
+secs=config.sections()
 
-# Functions to get and set config
-def get_cfg(sec, name):
-    return config.get(sec, name).strip('"')
+def get_cfg(sec,name):
+    return config.get(sec,name).strip('"')
 
-def set_cfg(sec, name, value):
-    config[sec][name] = '"%s"' % value
+def set_cfg(sec,name,value):
+    config[sec][name]='"%s"'%value
 
-# Function to get source and target languages
 def get_cfg_tra(sec):
-    cc = config.get(sec, "action").strip('"')
+    cc=config.get(sec,"action").strip('"')
+    target=""
+    source=""
     if cc == "auto":
         source  = 'auto'
         target  = 'zh-CN'
     else:
         source  = cc.split('->')[0]
         target  = cc.split('->')[1]
-    return source, target
+    return source,target
 
-# Directory for output
-BASE = get_cfg("cfg", 'base')
+BASE=get_cfg("cfg",'base')
 try:
     os.makedirs(BASE)
 except:
     pass
+links=[]
 
-links = []
 def tran(sec):
-    out_dir = BASE + get_cfg(sec, 'name')
-    url = get_cfg(sec, 'url')
-    max_item = int(get_cfg(sec, 'max'))
-    old_md5 = get_cfg(sec, 'md5')
-    source, target = get_cfg_tra(sec)
+    out_dir= BASE + get_cfg(sec,'name')
+    url=get_cfg(sec,'url')
+    max_item=int(get_cfg(sec,'max'))
+    old_md5=get_cfg(sec,'md5')
+    source,target=get_cfg_tra(sec)
     global links
 
-    links += [" - %s [%s](%s) -> [%s](%s)\n" % (sec, url, url, get_cfg(sec, 'name'), out_dir)]
+    links+=[" - %s [%s](%s) -> [%s](%s)\n"%(sec,url,url,get_cfg(sec,'name'),out_dir)]
 
-    new_md5 = get_md5_value(url) # new md5 value based on url
+    GT = Translate()
+    headers={
+        'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.137 Safari/537.36 LBBROWSER'
+        }
+    req = urllib.request.Request(url, headers=headers)
+
+    rss_doc=request.urlopen(req).read().decode('utf8')
+    new_md5= get_md5_value(rss_doc)
 
     if old_md5 == new_md5:
         return
     else:
-        set_cfg(sec, 'md5', new_md5)
+        set_cfg(sec,'md5',new_md5)
 
-    c = GoogleTran(url, target=target, source=source).get_newcontent(max=max_item)
+    soup = BeautifulSoup(rss_doc, 'xml')
+    items = soup.find_all('item')
+    for idx, e in enumerate(items):
+        if idx > max_item:
+            e.decompose()
 
-    with open(out_dir, 'w', encoding='utf-8') as f:
-        f.write(c)
-        print("GT: "+ url +" > "+ out_dir)
+    for idx, item in enumerate(soup.find_all('item')):
+        title = item.find('title').text
+        description = item.find('description').text
 
-for sec in secs:
-    if sec == "cfg":
-        continue
-    tran(sec)
+        translated_title = GT.translate(title, target=target, source=source).translatedText
+        translated_description = GT.translate(description, target=target, source=source).translatedText
+
+        item.find('title').string = translated_title
+        item.find('description').string = translated_description
+
+    content= str(soup)
+
+    with open(out_dir,'w',encoding='utf-8') as f:
+        f.write(content)
+    print("GT: "+ url +" > "+ out_dir)
+
+for x in secs[1:]:
+    tran(x)
+    print(config.items(x))
+
+with open('test.ini','w') as configfile:
+    config.write(configfile)
+
+YML="README.md"
+
+f = open(YML, "r+", encoding="UTF-8")
+list1 = f.readlines()
+f.seek(0)
+f.truncate()
+list1[2:2] = links
+list1 = "".join(list1)
+f.write(list1)
+f.close()
+print('Updated %s'%YML)
